0af24d272abb8392a21d806b0f6e071c7555d3dc
JesusZabdiel <A01374964@itesm.mx>
2020-03-17 18:43:15 -0600

    Add files via upload

---
+++ desktop/src/com/swing/itesm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.swing.itesm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.swing.itesm.Juego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Juego(), config);
+	}
+}
