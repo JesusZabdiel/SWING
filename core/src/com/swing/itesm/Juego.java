9b04025cabade9fd73ff3cd43e858e4ea3877250
A01377744 <A01377744@itesm.mx>
2020-05-21 14:50:44 -0500

    explicacion.png

--- core/src/com/swing/itesm/Juego.java
+++ core/src/com/swing/itesm/Juego.java
@@ -1,14 +1,8 @@
 package com.swing.itesm;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 
